e52526c4162c3326c90ba1ccf78c92cdc963073b
Behser Aj <besher.aj@outlook.com>
2020-11-07 16:59:11 +0800

    added models views and admin access for services"

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'image_url',
+        'quizz',
+    )
+
+    ordering = ('sku',)
+
+admin.site.register(Service, ServiceAdmin)
--- services/views.py
+++ services/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Lower
+
+from .models import Service 
 
 # Create your views here.
+def all_services(request):
+    services = Service.object.all()
+    context = {
+        'services':services
+    }
+    return render(request, 'services/service.html', context)
+
+def products_details(request, service_id):
+    service = get_object_or_404(Service, pk=service_id)
+    context = {
+        'services':service
+    }
+    return render(request, 'services/service_details.html', context)
