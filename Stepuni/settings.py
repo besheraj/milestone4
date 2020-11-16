76e14b62a96a939c529a4d8ffe5070430411ad83
Behser Aj <besher.aj@outlook.com>
2020-11-16 21:53:26 +0800

    depolyment attempt

--- Stepuni/settings.py
+++ Stepuni/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['stepuni.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['stepuni.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
