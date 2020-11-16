d3d5711997abaa925fc0e674a1fbe2a722474ef2
Behser Aj <besher.aj@outlook.com>
2020-11-16 21:27:11 +0800

    deployment attempt

--- Stepuni/urls.py
+++ Stepuni/urls.py
@@ -19,11 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/',include('allauth.urls')),
-    path('',include('home.urls')), 
-    path('profiles/',include('profiles.urls')), 
-    path('services/',include('services.urls')), 
-    path('checkout/',include('checkout.urls')), 
+                  path('admin/', admin.site.urls),
+                  path('accounts/', include('allauth.urls')),
+                  path('', include('home.urls')),
+                  path('profiles/', include('profiles.urls')),
+                  path('services/', include('services.urls')),
+                  path('checkout/', include('checkout.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
