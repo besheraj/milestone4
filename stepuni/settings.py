2947b16c703b310f32d6bc4f8eac877e3d9893ff
Besher Aboujeeb <besheraboujeeb@Beshers-MacBook-Pro.local>
2021-02-11 13:32:05 +0800

    settings update

--- stepuni/settings.py
+++ stepuni/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['stepuni-milestone4.herokuapp.com', '127.0.0.1']
 
