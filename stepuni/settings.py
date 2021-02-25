07d2a5c4702de60734298dff5d28241231607470
Besher <besher@Beshers-MacBook-Pro.local>
2021-02-26 00:49:17 +0800

    changed host name

--- stepuni/settings.py
+++ stepuni/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['stepuni-milestone4.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['stepuni.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
